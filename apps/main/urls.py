0a4e65d049e9eee81a8daa2ae882015b494084be
RuslanPodolian <Ruslan.podolian@gmail.com>
2024-02-10 00:37:47 +0200

    fix routing

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.index, name= 'index'),
     # path('lol', views.index, name='about'),
-    path('lol', TemplateView.as_view(template_name="main/about.html")),
+    path('about', TemplateView.as_view(template_name="main/about.html")),
     path('home', TemplateView.as_view(template_name="main/index.html"))
 ]
