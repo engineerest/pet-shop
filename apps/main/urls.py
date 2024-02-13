83130cc7dd79682afcead25ab8f8056d494a254c
RuslanPodolian <Ruslan.podolian@gmail.com>
2024-02-14 00:00:20 +0200

    fix routing, new routing

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name= 'index'),
     # path('lol', views.index, name='about'),
     path('about', TemplateView.as_view(template_name="main/about.html")),
-    path('home', TemplateView.as_view(template_name="main/index.html"))
+    path('home', TemplateView.as_view(template_name="main/index.html")),
+    path('for_us', TemplateView.as_view(template_name="main/for_us.html")),
+    path('contacts', TemplateView.as_view(template_name="main/contacts.html")),
+    path('veterinarsadvice', TemplateView.as_view(template_name="main/veterinarsadvice.html"))
 ]
